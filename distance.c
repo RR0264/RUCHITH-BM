6edbde247663aec2d99574be94040bff1287f4ff
ENG19CS0264 RUCHITH BM <76143152+RR0264@users.noreply.github.com>
2021-02-11 16:50:59 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,50 +1,38 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-int main()
+
+
+float calculateDistance(float x1,float y1, float x2,float y2)
+{
+return sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+}
+
+float getcordinates()
 {
 float x2,x1,y2,y1,d;
-float point1x();
-float point1y();
-float point2x();
-float point2y();
+printf("Enter the x coordinate of point one: ");
+scanf("%f",&x1);
+printf("Enter the y coordinate of point one: ");
+scanf("%f",&y1);
+printf("Enter the x coordinate of point two: ");
+scanf("%f",&x2);
+printf("Enter the y coordinate of point two: ");
+scanf("%f",&y2);
+d=calculateDistance(x1,y1,x2,y2);
+return d;
+}
 
-x1=point1x();
-y1=point1y();
 
-x2=point2x();
-y2=point2y();
-d=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-printf("Distance between the points =%f",d);
+int displaydistance(float d)
+{
+    printf("Distance between the points = %f ",d);
 return 0;
 }
 
-float point1x()
+int main()
 {
-float a;
-printf("Enter the x coordinate of point one");
-scanf("%f",&a);
-return a;
-}
-float point1y()
-{
-float a;
-printf("Enter the y coordinate of point one");
-scanf("%f",&a);
-return a;
-}
-
-float point2x()
-{
-float a;
-printf("Enter the x coordinate of point two");
-scanf("%f",&a);
-return a;
-}
-float point2y()
-{
-float a;
-printf("Enter the y coordinate of point two");
-scanf("%f",&a);
-return a;
+float d;
+d=getcordinates();
+displaydistance(d);
 }
