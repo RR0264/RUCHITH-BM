f32dfd52a31658fae5acf3243ca7f4a45862e558
ENG19CS0264 RUCHITH BM <76143152+RR0264@users.noreply.github.com>
2021-02-11 22:27:07 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -3,26 +3,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-struct coordinates {
-    int x, y;
-};
-
-double getDistance(struct coordinates a, struct coordinates b)
+typedef struct coordinates
 {
-    double d;
-    d = sqrt((a.x - b.x) * (a.x - b.x) + (a.y-b.y) *(a.y-b.y));
-    return d;
+  int x, y;
+} Coordinates;
+
+double
+getDistance (struct coordinates a, struct coordinates b)
+{
+  double d;
+  d = sqrt ((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+  return d;
 }
 
-int main()
+Coordinates
+input (Coordinates a,int c)
 {
-    double d;
-    struct coordinates a, b;
-    printf("Enter coordinate of point a: ");
-    scanf("%d %d", &a.x, &a.y);
-    printf("Enter coordinate of point b: ");
-    scanf("%d %d", &b.x, &b.y);
-    d=getDistance(a, b);
-    printf("Distance between a and b: %lf\n", d);
-    return 0;
+  printf ("Enter coordinate of point %d: ",c);
+  scanf ("%d %d", &a.x, &a.y);
+  return a;
+};
+
+void display(double d){
+  printf ("Distance between a and b: %lf\n", d);
+}
+
+int main ()
+{
+  double d;
+  struct coordinates a, b;
+  a = input (a,1);
+  b = input (b,2);
+  d = getDistance (a, b);
+  display(d);
+  return 0;
 }
