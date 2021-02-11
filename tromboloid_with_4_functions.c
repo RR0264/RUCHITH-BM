79e241bd28234e7b3e45293634717a5de355d2b0
ENG19CS0264 RUCHITH BM <76143152+RR0264@users.noreply.github.com>
2021-02-11 14:31:37 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,27 +1,41 @@
 //WAP to find the volume of a tromboloid using 4 functions.
-#include <stdio.h>
+#include<stdio.h>
+    float volume(float h,float b,float d)
+    {
+       return ((h*b*d)+(d/b))/3;
+     }
+     float getheight()
+     {
+         float h;
+        printf("Enter the h:");
+        scanf("%f",&h);
+        return h;
+     }
+     float getbreath()
+     {
+         float b;
+        printf("Enter the b:");
+        scanf("%f",&b);
+        return b;
+     }
+      float getdiameter()
+     {
+         float d;
+        printf("Enter the d:");
+        scanf("%f",&d);
+        return d;
+     }
 
-float take_input(){
-float temp;
-printf("Enter the three parameter of the tromboloid");
-scanf("%f",&temp);
-return temp;
-};
-
-float calc_vol(float h, float d, float b){
-return ((h *d*b)+(d/b))/3 ;
-};
-
-void show_output(float volume){
-printf("The Volume of the tromboloid is: %.2f", volume);
-
-};
-
-int main(){
-float h,d,b,vol;
-h = take_input();
-d =  take_input();
-b = take_input();
-vol = calc_vol(h, d, b);
-show_output(vol);
-return 0;}
+     float displaydata(float vol){
+        printf("Volume of the tromboloid is :%2f",vol);
+        return 0;
+     }
+     int main()
+     {
+          float h,b,d,vol;
+          h=getheight();
+          b=getbreath();
+          d=getdiameter();
+         vol=volume(h,b,d);
+         displaydata(vol);
+     }
